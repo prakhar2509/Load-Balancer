3dc366d8340401e245504b3998d4366f9c50d187
KanishkSogani <kanishksogani1@gmail.com>
2025-04-05 19:26:23 +0530

    update backend architecture for fixing errors

--- server/src/routes/gateway.routes.js
+++
@@ -1,7 +0,0 @@
-// import { Router } from "express";
-// import { handleRequest } from "../controllers/gateway.controller.js";
-
-// const gatewayRouter = Router();
-// gatewayRouter.all(handleRequest);
-
-// export default gatewayRouter;
